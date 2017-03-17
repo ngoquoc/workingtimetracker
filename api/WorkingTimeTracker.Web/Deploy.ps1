cb810301db152ad7dd91f25e93b1dc8c26b9f602
Quoc Ngo <quoc99@gmail.com>
2017-03-18 01:29:35 +0700

    Correct syntax error in Deploy script.

--- api/WorkingTimeTracker.Web/Deploy.ps1
+++ api/WorkingTimeTracker.Web/Deploy.ps1
@@ -1,6 +1,3 @@
 ﻿$connectionString = $OctopusParameters["SQL.WorkingTimeTracker.DeployConnectionString"]
 
-.\bin\migrate.exe WorkingTimeTracker.Implementations.dll
-	/connectionString="$($connectionString)"
-	/connectionProviderName="System.Data.SqlClient"
-	/verbose
+.\bin\migrate.exe WorkingTimeTracker.Implementations.dll /connectionString="$($connectionString)" /connectionProviderName="System.Data.SqlClient" /verbose
